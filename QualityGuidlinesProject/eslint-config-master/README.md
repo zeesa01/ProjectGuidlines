c18163b4678278bb94c86204eb1e20ab4a4a59e8
mohammad Zeeshan Alam <m.zeeshan.alam95@gmail.com>
2024-08-16 09:12:30 +0200

    Eslint rules config

---
+++ QualityGuidlinesProject/eslint-config-master/README.md
@@ -0,0 +1,27 @@
+# Eslint auto generated config for all ⚓
+
+A CLI to generate Eslint configuration with or without style guide like airbnb or standardjs
+
+The config can be generated in React, Typescript and NodeJS
+
+In your project run `npx linting.json` and pick your favorite config.
+
+Let's pick React as an example
+```javascript
+❯ React
+  Typescript 
+  NodeJS 
+```
+Pick the style guide
+```javascript
+❯ react-with-airbnb-config 
+  without-style-guide 
+  react-with-standard-js 
+```
+
+* [Note] The selected config will install all the dependencies
+
+
+@Todo
+- [ ] Add standard JS
+- [ ] Refactor the dependency installation
---
+++ QualityGuidlinesProject/eslint-config-master/src/index.js
@@ -0,0 +1,115 @@
+#!/usr/bin/env node
+const inquirer = require("inquirer");
+const { writeFileSync } = require("fs");
+const shell = require('shelljs');
+
+const reactAirbnbConfing = require("./config/react-with-airbnb-config.json");
+const tsLintConfig = require("./config/ts-eslint")
+const tsconfig = require("./config/ts-without-style-guide.json")
+const reactWithoutStyleGuid = require("./config/without-styleguide.json");
+const prettierConfig = require("./config/prettierrc.json");
+
+const eslintConfig = {
+    "react-with-airbnb-config": JSON.stringify(reactAirbnbConfing),
+    "without-style-guide": JSON.stringify(reactWithoutStyleGuid),
+    "ts-without-style-guide": JSON.stringify(tsconfig),
+};
+
+const configPromot = {
+    type: "list",
+    name: "eslint",
+    message:
+        "Please pick your favoriate eslint configuration along with prettier!",
+    choices: ["React", "Typescript", "NodeJS"],
+    filter: function (val) {
+        return val.toLowerCase();
+    },
+};
+
+const reactPromot = {
+    type: "list",
+    name: "react",
+    message: "Please choose your eslint style guide!",
+    choices: [
+        "react-with-airbnb-config",
+        "without-style-guide",
+        "react-with-standard-js",
+    ],
+};
+const typescriptPromot = {
+    type: "list",
+    name: "typescript",
+    message: "Please choose your typescript eslint style guide!",
+    choices: [
+        "ts-without-style-guide",
+        "typescript-react"
+    ],
+};
+
+
+function main() {
+    inquirer.prompt(configPromot).then(({ eslint }) => {
+        if (eslint === "react") {
+            reactConfig();
+        } else if (eslint === "typescript") {
+            typeScriptConfig();
+        } else {
+            console.log(
+                "==== Sorry, NodeJS version Will be added to the next version ======"
+            );
+        }
+    });
+}
+
+function typeScriptConfig() {
+    inquirer.prompt(typescriptPromot).then(({ typescript }) => {
+        let choice = eslintConfig[typescript];
+
+        if (typescript === "typescript-react") {
+            console.log("==== Will be added to the next version ======");
+        } else {
+            const cwd = process.cwd();
+            console.log("==== Installing dependencies ====== ");
+            shell.exec('yarn add --dev typescript')
+            shell.exec('yarn add --dev @typescript-eslint/parser')
+            shell.exec('yarn add --dev prettier')
+            writeFileSync(cwd + "/.eslintrc", JSON.stringify(tsLintConfig));
+            writeFileSync(cwd + "/.prettierrc", JSON.stringify(prettierConfig));
+            writeFileSync(cwd + "/tsconfig.json", choice);
+            console.log(
+                "\x1b[32m",
+                `🎉 🎉 Eslint and prettier config successfully generated for ${typescript} 🎉 🎉 `
+            );
+        }
+    });
+}
+
+function reactConfig() {
+    inquirer.prompt(reactPromot).then(({ react }) => {
+        let choice = eslintConfig[react];
+
+        if (react === "react-with-standard-js") {
+            console.log("==== Will be added to the next version ======");
+        } else {
+            const cwd = process.cwd();
+            console.log("==== Installing dependencies ====== ");
+            shell.exec('yarn add --dev eslint')
+            shell.exec('yarn add --dev eslint-config-airbnb')
+            shell.exec('yarn add --dev prettier')
+            shell.exec('yarn add --dev babel-eslint')
+            shell.exec('yarn add --dev eslint-plugin-react')
+            shell.exec('yarn add --dev eslint-plugin-import')
+            shell.exec('yarn add --dev eslint-plugin-jsx-a11y')
+            shell.exec('yarn add --dev eslint-config-prettier')
+            shell.exec('yarn add --dev eslint-plugin-react-hooks')
+            writeFileSync(cwd + "/.eslintrc", choice);
+            writeFileSync(cwd + "/.prettierrc", JSON.stringify(prettierConfig));
+            console.log(
+                "\x1b[32m",
+                `🎉 🎉 Eslint and prettier config successfully generated for ${react} 🎉 🎉 `
+            );
+        }
+    });
+}
+
+main();
